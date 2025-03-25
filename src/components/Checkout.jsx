589e4e4096a61f9d7c2c6197a6bc6641494e03c7
Yuvraj Nirwan <yuvichauhan2000@gmail.com>
2025-03-25 19:36:17 +0530

    minor changes in checkout page

--- src/components/Checkout.jsx
+++ src/components/Checkout.jsx
@@ -3,48 +3,42 @@ import "./Checkout.css";
 
 function Checkout() {
   return (
-    <div>
-      <h1>Checkout</h1>
-      <p>Thank you for shopping with us!</p>
-      <div className="checkout">
+  
+      <div className="checkout-box">
+        <h1>Checkout</h1>
+        <p>Thank you for shopping with us!</p>
         <form>
-          <label htmlFor="name">Name:</label>
-          <input type="text" name="fname" placeholder="Enter your name" />
-          <br />
-
-          <label htmlFor="phone">Phone no:</label>
-          <input
-            type="text"
-            name="phone"
-            placeholder="Enter your phone number"
-          />
-          <br />
-
-          <label htmlFor="address">Address:</label>
-          <input type="text" name="address" placeholder="Enter your address" />
-          <br />
-
-          <label htmlFor="city">City:</label>
-          <input type="text" name="city" placeholder="Enter your city" />
-          <br />
-
-          <label htmlFor="state">State:</label>
-          <input type="text" name="state" placeholder="Enter your state" />
-          <br />
-
-          <label htmlFor="landmark">Landmark:</label>
-          <input type="text" name="landmark" placeholder="Landmark" />
-          <br />
-
-          <button
-            type="button"
-            onClick={() => alert("Order placed successfully")}
-          >
-            Place Order
-          </button>
+          <label>Name:</label>
+          <input type="text" placeholder="Enter your name" />
+          
+          <label>Phone no:</label>
+          <input type="text" placeholder="Enter your phone number" />
+          
+          <label>Address:</label>
+          <input type="text" placeholder="Enter your address" />
+          
+          <label>City:</label>
+          <input type="text" placeholder="Enter your city" />
+          
+          <label>State:</label>
+          <input type="text" placeholder="Enter your state" />
+          
+          <label>Landmark:</label>
+          <input type="text" placeholder="Landmark" />
+          
+          <label></label>
+          <div className="payment-options">
+          Payment type:
+            <input type="radio" name="payment" /> Wallet
+            <input type="radio" name="payment" /> UPI
+            <input type="radio" name="payment" /> Netbanking
+            <input type="radio" name="payment" /> Cash
+          </div>
+          <div>
+          <button type="button" className="order-btn" onClick={() => alert("Order placed successfully")}>Place Order</button>
+          </div>
         </form>
       </div>
-    </div>
   );
 }
 
