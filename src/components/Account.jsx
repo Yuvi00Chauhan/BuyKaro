e84a33596dec1b5e5fd303c6ef027b16dd46c0c3
Yuvraj Nirwan <yuvichauhan2000@gmail.com>
2025-04-16 19:12:16 +0530

    updates

--- src/components/Account.jsx
+++ src/components/Account.jsx
@@ -13,7 +13,7 @@ function Account() {
     return (
         <div className="account">
             <div className="top-content">
-                <img className="user-img" src="/images/me.jpg" alt="User Avatar" />
+                <img className="user-img" src="../images/me.jpg" alt="User Avatar" />
                 <h1 className="acc-head">Developed by Yuvraj Nirwan</h1>
                 <a className="github-link" href="https://github.com/Yuvi00Chauhan" target="_black">@Yuvi00Chauhan</a>
                 <br/>
